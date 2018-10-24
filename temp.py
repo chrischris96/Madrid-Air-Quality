49affa1cce6fb53114c9bcc4c657a7719c837c3b
chrischris96 <christian.plitz@gmx.de>
2018-10-25 00:43:47 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,6 +4,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#from matplotlib.patches import Polygon
+#from matplotlib.collections import PatchCollection
+#from matplotlib.colors import Normalize
+from shapely.geometry import Point, LineString
+
 path = r'C:\Users\Chris\Desktop\Machine Learning Raw Data\air-quality-madrid\stations.csv'
 stations = pd.read_csv(path)
 stations.head()
@@ -23,9 +28,9 @@ streetselected = calleandavd.append(ctraselected)
 #xlim=(420000,460000)
 #ylim=(4460000,4490000)
 
-base = geoDF_stations_new.plot(figsize=(32,20), marker='o',color='red',markersize=100.0,label='Stations')
+base = geoDF_stations_new.plot(figsize=(32,20), marker='o',color='red',markersize=100.0,label='Stations');
 
-mapMadrid = streetselected.plot(figsize=(32,20), ax=base,color='blue', edgecolor='blue',markersize=0.01,label='Streets') 
+mapMadrid = streetselected.plot(figsize=(32,20), ax=base,color='blue', edgecolor='blue',markersize=0.01,label='Streets');
 
 plt.ylim((4465000,4485000))
 plt.xlim((430000,455000))
@@ -33,4 +38,4 @@ plt.legend(loc = 'lower right', framealpha=1)
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.title("Madrid city center street map with measurement stations")
-plt.plot(mapMadrid)
+plt.show(mapMadrid)
